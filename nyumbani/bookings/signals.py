5da63583b99af2192bf97d64d6f8affa96270402
Joseph Musembi <musembinzioki@gmail.com>
2024-12-03 13:51:56 +0300

    alert admins on new bookings

--- nyumbani/bookings/signals.py
+++ nyumbani/bookings/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.conf import settings
 from django.dispatch import receiver
 from bookings.models import Booking
+from organizations.models import UserOrganization
 
 from comms.sms import send_bunicom_sms, send_sms_to_console
 
@@ -13,9 +14,23 @@ def send_booking_sms(sender, instance, created, **kwargs):
     end_time = instance.end_time.strftime("%Y-%m-%d %H:%M:%S")
     booked_by = instance.booked_by.name
     phone_number = instance.booked_by.phone_number
+    
+    # admin message
+    organization = instance.organization
+    admin_phone_numbers = UserOrganization.objects.filter(organization=organization, is_admin=True).values_list(
+        'user__phone_number', flat=True
+    )
+    admin_phone_numbers = list(admin_phone_numbers)
+    admin_message = f"Hello. There is a new booking by {booked_by} for {room} from {start_time} to {end_time}."
+    
+
     if created:
         message = f"Hello {booked_by}, you have successfully booked {room} from {start_time} to {end_time}."
         if settings.ENVIRONMENT == "PRODUCTION":
             send_bunicom_sms(message, [phone_number])
+            send_bunicom_sms(admin_message, admin_phone_numbers)
+
             return
+
         send_sms_to_console(message, [phone_number])
+        send_bunicom_sms(admin_message, admin_phone_numbers)
